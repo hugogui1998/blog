b9ea559404c6aec486f1f2b1bf6f0eb971d6d6d9
hugogui1998 <L161080071@iztapalapa.tecnm.mx>
2020-08-05 23:15:28 -0500

    Creacion del formulario entradas

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/entries/create', 'EntryController@create')->name('create');
+Route::post('/entries', 'EntryController@store')->name('store');
